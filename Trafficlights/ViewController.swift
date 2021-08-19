c0477eb2463ae78391203e03c0596fae9742b933
Dmitri Gordeev <dimapushkin@gmail.com>
2021-08-19 07:53:44 +0300

    setup mainboard and logic

--- Trafficlights/ViewController.swift
+++ Trafficlights/ViewController.swift
@@ -9,11 +9,81 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yelowwLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var pushButton: UIButton!
+    
+    enum LightColor {
+        case red
+        case yellow
+        case green
+        case off
+    }
+    
+    var lightColorSet: LightColor = .off
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.alpha = 0.3
+        yelowwLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        redLight.layer.masksToBounds = true
+        
+        yelowwLight.layer.cornerRadius = yelowwLight.frame.width / 2
+        yelowwLight.layer.masksToBounds = true
+        
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        greenLight.layer.masksToBounds = true
+        
+        pushButton.layer.cornerRadius = 10
+      
+       
     }
 
-
+    @IBAction func tappedPushButton() {
+    pushButton.setTitle("NEXT", for: .normal)
+    
+  
+    
+    
+    switch lightColorSet {
+    case .red:
+        pushButton.setTitle("NEXT", for: .normal)
+        
+        redLight.alpha = 1
+        yelowwLight.alpha = 0.3
+        greenLight.alpha = 0.3
+      
+        lightColorSet = .yellow
+        
+    case .yellow:
+        pushButton.setTitle("NEXT", for: .normal)
+        
+        redLight.alpha = 0.3
+        yelowwLight.alpha = 1
+        greenLight.alpha = 0.3
+        
+        lightColorSet = .green
+        
+    case .green:
+        pushButton.setTitle("NEXT", for: .normal)
+        
+        redLight.alpha = 0.3
+        yelowwLight.alpha = 0.3
+        greenLight.alpha = 1
+        
+        lightColorSet = .red
+  
+    case .off:
+        lightColorSet = .red
+    }
+   
+    }
+    
 }
 
